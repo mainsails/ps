487b164eeea5cb73a3decbc395fd930dff3a9e86
Sam-AtWork <Sam-AtWork@users.noreply.github.com>
2017-06-28 15:17:10 +0100

    Update-BIOS - $TSEnv Logging
    Added CMTrace compatible logging to the script.
    Logs are stored along with all other OSD logging

--- Development/BIOS/BIOS-Update/Update-BIOS.ps1
+++ Development/BIOS/BIOS-Update/Update-BIOS.ps1
@@ -1,26 +1,77 @@
-    $BIOSStructure = Split-Path $MyInvocation.MyCommand.Path -Parent
-    $SystemInfo    = Get-WmiObject -Class Win32_ComputerSystem
-    $BIOSInfo      = Get-WmiObject -Class Win32_BIOS
-    $Manufacturer  = $SystemInfo.Manufacturer
-    $Model         = $SystemInfo.Model
-    $BIOSvCurrent  = $BIOSInfo.SMBIOSBIOSVersion
-    $BIOSSource    = Get-ChildItem -Path $BIOSStructure -Include $Model -Recurse
+Function Write-CMTraceLog {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Message,
+        [int]$ProcessID = $PID,
+        [Parameter()]
+        [ValidateSet(1, 2, 3)]
+        [int]$LogLevel = 1
+    )
+    $TimeGenerated = "$(Get-Date -Format HH:mm:ss).$((Get-Date).Millisecond)+000"
+    $Line          = '<![LOG[{0}]LOG]!><time="{1}" date="{2}" component="{3}" context="" type="{4}" thread="{5}" file="">'
+    $LineFormat    = $Message, $TimeGenerated, (Get-Date -Format MM-dd-yyyy), "$($MyInvocation.ScriptName | Split-Path -Leaf):$($MyInvocation.ScriptLineNumber)", $LogLevel, $ProcessID
+    $Line          = $Line -f $LineFormat
+    Add-Content -Value $Line -Path $Log
+}
+
+# Script Start
+$TSEnv   = New-Object -COMObject Microsoft.SMS.TSEnvironment
+$LogPath = $TSEnv.Value('LOGPATH')
+$Log     = "$LogPath\$(([io.fileinfo]$MyInvocation.MyCommand.Definition).BaseName).log"
+
+Write-CMTraceLog "BIOS Update Scan Starting"
+
+$BIOSPassword  = 'm8E1StR0m'
+
+$BIOSStructure = Split-Path $MyInvocation.MyCommand.Path -Parent
+$SystemInfo    = Get-WmiObject -Class Win32_ComputerSystem
+$BIOSInfo      = Get-WmiObject -Class Win32_BIOS
+$Manufacturer  = $SystemInfo.Manufacturer
+$Model         = $SystemInfo.Model
+$BIOSvCurrent  = $BIOSInfo.SMBIOSBIOSVersion
+$BIOSSource    = Get-ChildItem -Path $BIOSStructure -Include $Model -Recurse
+
+Write-CMTraceLog "BIOS Update Folder Structure set to $BIOSStructure"
+Write-CMTraceLog "WMI Lookup on Win32_ComputerSystem : $SystemInfo"
+Write-CMTraceLog "WMI Lookup on Win32_BIOS : $BIOSInfo"
+Write-CMTraceLog "Manufacturer Detected as $Manufacturer"
+Write-CMTraceLog "Model Detected as $Model"
+Write-CMTraceLog "Current BIOS Version Detected as $BIOSvCurrent"
 
 If ($BIOSSource) {
+    Write-CMTraceLog "BIOS Update Source set to $BIOSSource"
+    Write-CMTraceLog "Scanning Update Source for BIOS versions"
     $BIOSvLatest = Get-ChildItem -Path $BIOSSource -Filter '*.ver' | Select -ExpandProperty BaseName
+    Write-CMTraceLog "Archived BIOS Version Detected as : $BIOSvLatest"
     If ($BIOSvCurrent -ne $BIOSvLatest) {
+        Write-CMTraceLog "Machine BIOS does not match Archived BIOS"
+        Write-CMTraceLog "Updating BIOS from $BIOSvCurrent to $BIOSvLatest"
+        Write-CMTraceLog "Scanning Update Source for BIOS executable"
         $UpdateExe = (Get-ChildItem -Path $BIOSSource -Filter *.exe).Name
-        If ($UpdateExe.Count -ne 1) {Break}
+        If ($UpdateExe.Count -ne 1) {
+            Write-CMTraceLog "Multiple Executables found in $BIOSSource - Exiting" -LogLevel 3
+            Break
+        }
+        Else {
+            Write-CMTraceLog "BIOS Update Executable detected as : $UpdateExe"
+        }
         Copy-Item -Path $BIOSSource -Destination $env:TEMP -Recurse -Force
         Set-Location -Path $env:TEMP\$Model
+        Write-CMTraceLog "New BIOS copied from $BIOSSource to $env:TEMP\$Model)"
+        Write-CMTraceLog "BIOS Executable Vendor detected as : $($BIOSSource.Parent.Name)"
         If ($BIOSSource.Parent.Name -eq 'Dell') {
-            $UpdateArgs = '/s /f /p=m8obfuscated'
+            $UpdateArgs = "/s /f /p=$BIOSPassword"
         }
         ElseIf ($BIOSSource.Parent.Name -eq 'HP') {
-            $UpdateArgs = ''
+            $UpdateArgs = 'TO DO'
         }
-        Else {Break}
+        Else {
+            Write-CMTraceLog "No Executable Arguments for $($BIOSSource.Parent.Name) are defined -LogLevel 3"
+        }
+        Write-CMTraceLog "Launching BIOS Update : $UpdateEXE"
         $BIOSUpdate = Start-Process -FilePath $env:TEMP\$Model\$UpdateEXE -NoNewWindow -Wait -Passthru -ArgumentList $UpdateArgs
+        Write-CMTraceLog "Stopped BIOS Update : $UpdateEXE with Return Code $($BIOSUpdate.ExitCode)" -ProcessID $($BIOSUpdate.Id)
+        Write-CMTraceLog "Retrieving Return Codes for $($BIOSSource.Parent.Name) Update Executable"
         If ($BIOSSource.Parent.Name -eq 'Dell') {
             Switch ($BIOSUpdate.ExitCode) {
                 '0' {
@@ -54,18 +105,25 @@ If ($BIOSSource) {
             }
         }
         If ($BIOSUpdateSuccess -eq $true) {
-            # Success - Requires Reboot
+            Write-CMTraceLog "BIOS Update Returned : $BIOSUpdateRtnMsg"
+            $TSEnv.Value('BIOSUpdateRestart') = $true
         }
         ElseIf ($BIOSUpdateSuccess -eq $false) {
+            Write-CMTraceLog "BIOS Update Returned : $BIOSUpdateRtnMsg" -LogLevel 3
         }
         Else {
-            # Unknown Result
+            Write-CMTraceLog "BIOS Update Returned Unknown Result" -LogLevel 2
         }
     }
     Else {
-        # Machine BIOS matches Archived BIOS
+        Write-CMTraceLog "Machine BIOS matches Archived BIOS : $BIOSvCurrent"
     }
 }
 Else {
-    # No Archived BIOS available for this machine
+    Write-CMTraceLog "No Archived BIOS Version Information available for this machine" -LogLevel 2
 }
+If ($TSEnv.Value('BIOSUpdateRestart') -eq $true) {
+    Write-CMTraceLog "Restart required"
+    Write-CMTraceLog "Task Sequence Variable set to enforce restart"
+}
+Write-CMTraceLog "BIOS Update Scan Finished"
