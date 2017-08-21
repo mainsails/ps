ca46ac0fecbc478de48c174544917f852c6522ce
Sam <mainsails@users.noreply.github.com>
2017-08-21 12:19:09 +0100

    Add Function 'Import-ModuleFromGitHub.ps1'
    Allows quick imports of PowerShell Modules directly from (raw) GitHub code repositories

---
+++ Import-ModuleFromGitHub.ps1
@@ -0,0 +1,57 @@
+Function Import-ModuleFromGitHub {
+    <#
+    .SYNOPSIS
+        Imports a PowerShell Module from a GitHub repository
+    .DESCRIPTION
+        Downloads and imports a PowerShell Module directly from a GitHub repository
+    .PARAMETER Uri
+        The Uri of the GitHub hosted PowerShell Module (raw psm1)
+    .EXAMPLE
+        Import-ModuleFromGitHub -Uri 'https://raw.githubusercontent.com/mainsails/ps/master/ApplicationManagement.psm1'
+    .EXAMPLE
+        $Uri = 'https://raw.githubusercontent.com/mainsails/ps/master/ApplicationManagement.psm1'
+        Import-ModuleFromGitHub -Uri $Uri -Verbose
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [ValidateScript({ ($_ -match '^https?') })]
+        [Alias('Url')]
+        [string]$Uri
+    )
+
+    Begin {
+        # Verbose Logging
+        [string]$CmdletName  = $MyInvocation.MyCommand.Name
+        [string]$CmdletParam = $PSBoundParameters | Format-Table -Property @{ Label = 'Parameter'; Expression = { "[-$($_.Key)]" } }, @{ Label = 'Value'; Expression = { $_.Value }; Alignment = 'Left' } -AutoSize -Wrap | Out-String
+        Write-Verbose -Message "##### Calling : [$CmdletName]"
+
+        # URL RegEx Pattern
+        [regex]$UriRegExPattern = '^(https?)'
+    }
+    Process {
+        Try {
+            $Module = Split-Path -Path $Uri -Leaf
+            $Destination = $env:TEMP + '\' + $Module
+            Try {
+                # Download PowerShell Module
+                Write-Verbose -Message "Downloading PowerShell Module [$Module] from Uri [$Uri]"
+                Invoke-WebRequest -Uri $Uri -UseBasicParsing -OutFile $Destination -ErrorAction Stop
+                # Import PowerShell Module
+                Write-Verbose -Message "Importing PowerShell Module [$Module] from File [$Destination]"
+                Import-Module -Name $Destination -ErrorAction Stop
+            }
+            Catch {
+                Throw "Failed to download module : $($_.Exception.Message)"
+            }
+        }
+        Catch {
+            Throw "Failed to import module : $($_.Exception.Message)"
+        }
+    }
+    End {
+        # Verbose Logging
+        Write-Verbose -Message "##### Ending : [$CmdletName]"
+    }
+}
