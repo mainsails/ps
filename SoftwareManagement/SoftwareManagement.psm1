4c8b2a7e4c0ecaf1bbe058480dba99a274c32763
Sam <Sam-AtWork@users.noreply.github.com>
2017-09-08 16:17:45 +0100

    SoftwareManagement Module Refactor
    Initial upload cont.

--- SoftwareManagement/SoftwareManagement.psm1
+++ SoftwareManagement/SoftwareManagement.psm1
@@ -1,11 +1,11 @@
 # Import C#
-Add-Type -Path "C:\Users\shaws\Desktop\SoftwareManagement\TypeData\PSSM-Msi.cs"
-Add-Type -Path "C:\Users\shaws\Desktop\SoftwareManagement\TypeData\PSSM-Explorer.cs"
+Add-Type -Path "$PSScriptRoot\TypeData\PSSM-Msi.cs"
+Add-Type -Path "$PSScriptRoot\TypeData\PSSM-Explorer.cs"
 $ReferencedAssemblies = ('System.DirectoryServices, Version=2.0.0.0, Culture=neutral, PublicKeyToken=B03F5F7F11D50A3A')
-Add-Type -Path "C:\Users\shaws\Desktop\SoftwareManagement\TypeData\PSSM-QueryUser.cs" -ReferencedAssemblies $ReferencedAssemblies
+Add-Type -Path "$PSScriptRoot\TypeData\PSSM-QueryUser.cs" -ReferencedAssemblies $ReferencedAssemblies
 
 # Get public and private function definition files
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\*.ps1 -ErrorAction SilentlyContinue )
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 # Dot source the files
