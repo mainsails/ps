661b406f40e8d3aff860307c9d75bef3a744cc5f
mainsails <mainsails@users.noreply.github.com>
2017-11-01 13:06:24 +0000

    Add 'Add-UserToGroup.ps1' for reusability
    Remove 'Add-UserToUAG.ps1'
    Replace with 'Add-UserToGroup.ps1'
    Refactored for reusability

--- Development/Add-UserToGroup.ps1
+++ Development/Add-UserToGroup.ps1
@@ -1,27 +1,31 @@
-Function Add-UserToUAG {
+Function Add-UserToGroup {
     <#
     .Synopsis
-        Adds one or more users to the UAG Group
+        Adds one or more users to a Group
     .Description
-        Adds one or more users to the UAG Group - 'UAG - Portal Pilot Users'
+        Adds one or more users to an Active Directory Group
     .Parameter Username
-        List of usernames to add to the UAG Group
+        List of usernames to add to the Group
+    .Parameter Username
+        List of usernames to add to the Group
     .Example
-        Add-UserToUAG -UserName "UserName1", "UserName2", "UserName3"
+        Add-UserToGroup -UserName "UserName1", "UserName2", "UserName3"
     .Example
-        Get-Clipboard | Add-UserToUAG
+        Get-Clipboard | Add-UserToGroup
     #>
     #Requires -Module ActiveDirectory
 
     Param (
         [Parameter(ValueFromPipeline=$true)]
-        [String[]]$UserName
+        [String[]]$UserName,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({ Get-ADGroup -Identity $_ })]
+        $Group
     )
 
     Begin {
         ## Set Group
-        $Group = 'UAG Group'
-        $Group = Get-ADGroup $Group
+        $Group = Get-ADGroup -Identity $Group
         # Create ArrayList for Users
         $UserList = New-Object -TypeName System.Collections.ArrayList
     }
@@ -32,7 +36,7 @@ Function Add-UserToUAG {
     End {
         ## Get AD Users
         # Trim whitespace, remove duplicates
-        $UserList = $UserList | ForEach-Object { $_.Trim() } | Sort-Object -Unique
+        $UserList = $UserList | ForEach-Object { If ($_) { $_.Trim() | Sort-Object -Unique }}
         # Build AD Filter
         $Filter = ($UserList | ForEach-Object { "(SamAccountName -eq '$_' -and MemberOf -ne '$($Group.DistinguishedName)')" }) -join ' -or '
         # Get AD Users that aren't a member of the Group
